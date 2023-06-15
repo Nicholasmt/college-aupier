a1bdadf36733b389fef518102bf022205c9ee388
mba <Nicholasmt>
2023-06-15 10:15:03 +0100

    updated slider header

--- resources/views/front-layouts/slider.blade.php
+++ resources/views/front-layouts/slider.blade.php
@@ -4,8 +4,8 @@
     <div class="row justify-content-center">
     <div class="col-md-12 col-lg-9 col-xs-12 text-center">
     <div class="contents mt-4">
-    <h1 class="head-title">US Universities Admissions <span class="year">Support</span></h1>
-    <p>From the Ivy League to Stanford, MIT and many more... <br>
+    <h1 class="head-title hero-area-title">US Universities Admissions <span class="year">Support</span></h1>
+    <p class="hero-area-sub-title">From the Ivy League to Stanford, MIT and many more... <br>
        Crimson helps students reach their ultimate college admissions goals.
     </p>
     <div class="mt-5">
